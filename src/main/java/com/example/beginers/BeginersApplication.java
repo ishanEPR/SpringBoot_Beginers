d4239657703e93515b147543cdc285f3a5db384c
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-08-10 00:47:03 +0530

    first commit

---
+++ src/main/java/com/example/beginers/BeginersApplication.java
@@ -0,0 +1,13 @@
+package com.example.beginers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeginersApplication.class, args);
+	}
+
+}
