eef9a7a7b36e93c9d844ced338d990d18cf7461a
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-08-10 11:18:31 +0530

    implement interface

---
+++ src/main/java/com/example/beginers/service/DepartmentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.beginers.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.beginers.entity.Department;
+import com.example.beginers.repository.DepartmentRepository;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	@Override
+	public Department saveDepartment(Department department) {
+		
+		return departmentRepository.save(department);
+	}
+
+	@Override
+	public List<Department> fetchDepartmentList() {
+		
+		return departmentRepository.findAll();
+	}
+
+	@Override
+	public Department fetchDepartmentById(Long departmentId) {
+		
+		return departmentRepository.findById(departmentId).get();
+	}
+	
+	
+
+}
